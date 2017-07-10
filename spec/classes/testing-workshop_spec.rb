a2425ae28831a3100d80d43a5f70fbde6cd2fe26
Martin Alfke <ma@example42.com>
2017-07-10 12:31:08 +0200

    updat etests

--- spec/classes/testing-workshop_spec.rb
+++ spec/classes/testing-workshop_spec.rb
@@ -1,14 +1,36 @@
 require 'spec_helper'
 
-describe 'frankfurt-workshop', :type => :class do
+describe 'testing_workshop', :type => :class do
   on_supported_os.each do |os, facts|
-    context "it should compile on #{os}" do
+    context "it should compile on #{os} with default parameters" do
       let(:facts) do
         facts
       end
+
       it { should compile.with_all_deps }
       it { should contain_package('less') }
       it { should contain_file('/etc/lessrc') }
+    end
+    context "it should compile on #{os} with validate set to true" do
+      let(:facts) do
+        facts
+      end
+      let (:params) do
+        { :validate => true }
+      end
+
+      it { should compile.with_all_deps }
+      it { should contain_notify('validation enabled') }
+    end
+    context "it should compile on #{os} with validate set false parameter" do
+      let(:facts) do
+        facts
+      end
+      let (:params) do
+        { :validate => 'foo' }
+      end
+
+      it { is_expected.to compile.and_raise_error(/expects a Boolean value/) }
     end
   end
 end
